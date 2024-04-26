099c3e910e69e8bbb198e0de5cbca11b0c81fc27
rar34 <ra.cse34@gmail.com>
2024-04-26 23:57:25 +0600

    added logout functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../FirebaseProvider/FirebaseProvider";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
     const [theme, setTheme] = useState('light')
@@ -17,6 +19,20 @@ const Navbar = () => {
             setTheme('light')
         }
     }
+
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(result => {
+                toast("log out successful")
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     const navLinks = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/all-tourists-spot">All Tourists Spot</NavLink></li>
@@ -26,7 +42,7 @@ const Navbar = () => {
 
 
     return (
-        <div className="navbar shadow-md bg-base-100">
+        <div className="navbar my-4 md:px-20 shadow-md bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -44,9 +60,25 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link to="/login"><button className="btn btn-success">Login</button></Link>
-                <Link to="/logout"><button className="btn btn-success">Logout</button></Link>
-                <Link to="/register"><button className="btn btn-success">Register</button></Link>
+                {user ?
+                    <div className="dropdown dropdown-end " >
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar tooltip" data-tip={user?.displayName || user?.email}>
+                            <div className="w-10 rounded-full ">
+                                <img alt="Tailwind CSS Navbar component" src={user?.photoURL || "https://i.ibb.co/ZX6HMzF/pp.jpg"} />
+                            </div>
+
+                        </div>
+                        <ul tabIndex={0} className="mt-3 z-[10] p-2 shadow menu menu-sm text-white font-bold dropdown-content bg-neutral w-32">
+                            <li className="hover:bg-gray-500"><Link to="/user-profile">User Profile</Link></li>
+                            <li className="hover:bg-gray-500"><Link to="/login"><button onClick={handleLogOut}>Logout</button></Link></li>
+                        </ul>
+                    </div>
+                    :
+                    <>
+                        <Link to="/login"><button className="btn btn-success text-white mr-3">Login</button></Link>
+                        {/* <Link to="/register"><button className="btn btn-success text-white">Register</button></Link> */}
+                    </>
+                }
                 <label className="cursor-pointer grid place-items-center">
                     <input onChange={handleToggle} type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                     <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
