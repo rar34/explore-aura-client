afd8be4aefb333de7fb463c13ad0a547d2489cfe
rar34 <ra.cse34@gmail.com>
2024-04-28 17:41:20 +0600

    added deleted function in my list compopent

--- src/components/Tourists/MyList.jsx
+++ src/components/Tourists/MyList.jsx
@@ -1,13 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../FirebaseProvider/FirebaseProvider";
+import Swal from "sweetalert2";
 
 const MyList = () => {
     const { user } = useContext(AuthContext);
     const [places, setPlaces] = useState([]);
-    console.log(user.email)
-    console.log(places)
-
-    // const { image } = places;
+    
 
     useEffect(() => {
         fetch(`http://localhost:5000/touristPlace/${user?.email}`)
@@ -17,6 +15,35 @@ const MyList = () => {
                 setPlaces(data)
             });
     }, [user])
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/touristPlace/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your place has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
     return (
         <div className="px-4 md:px-36 my-10">
             <h2 className="text-3xl text-center font-bold my-14 border-2 rounded-lg shadow-md p-5">My List</h2>
@@ -30,7 +57,7 @@ const MyList = () => {
                             <p>{place.short_description}</p>
                             <div className="flex gap-2 md:gap-4 justify-end">
                                 <button className="btn text-white btn-success">Update</button>
-                                <button className="btn text-white btn-success">Delete</button>
+                                <button onClick={() => handleDelete(place._id)} className="btn text-white btn-success">Delete</button>
                             </div>
                         </div>
                     </div>
